5a19872f8bae0be9fc4e8dc4b47b5c72d2a7db76
mkdorff <mkdorff@outlook.com>
2018-05-24 23:20:25 -0600

    added some more globals, using seed now, FINALLY got menu on desktop how i want it. Also did some TMNT does on the side

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -7,27 +7,20 @@ export default class Menu extends Component {
   }
 
   render() {
-    const {seed, active} = this.props;
+    const {seed, showMenu} = this.props;
 
     return (
       <div className='menu'>
-        <div className='menu-item' onClick={this.handleClick}>WIKIPEDIA NERL</div>
-        <div className='menu-item' onClick={this.handleClick}>TETRIS</div>
-        <div className='menu-item' onClick={this.handleClick}>YUKI DORFF</div>
-        <div className='menu-item' onClick={this.handleClick}>ETHERNET TETHERED SUBMARINE</div>
-        <div className='menu-item' onClick={this.handleClick}>HIPPO HIPPO</div>
-        <div className='menu-item' onClick={this.handleClick}>WINDO</div>
-        <div className='menu-item' onClick={this.handleClick}>SUDOKU</div>
-        <div className='menu-item' onClick={this.handleClick}>ASTEROIDS</div>
-        <div className='menu-item' onClick={this.handleClick}>MACHINE LEARNING ALGORITHM IMPLEMENTATIONS</div>
-        <div className='menu-item' onClick={this.handleClick}>MAPOVER</div>
-        <div className='menu-item' onClick={this.handleClick}>PHYSICS PROJECTS</div>
-        <div className='menu-item' onClick={this.handleClick}>RSA: JS & EMACS LISP</div>
-        <div className='menu-item' onClick={this.handleClick}>MANGA WEB SCRAPER</div>
-        <div className='menu-item' onClick={this.handleClick}>DEDUPE ICS EVENTS</div>
-        <div className='menu-item' onClick={this.handleClick}>GOOGLE MUSIC TO LASTFM SYNC</div>
-        <div className='menu-item' onClick={this.handleClick}>SPOTIFY</div>
-        <div className='menu-item' onClick={this.handleClick}>STUDENT STATS IN GOOGLE SHEETS</div>
+        {seed.map((item, idx) => (
+          <div 
+            className='menu-item' 
+            onClick={this.handleClick} 
+            key={idx}>
+            <div className='item-text'>
+              {item.longName}
+            </div>
+          </div>
+        ))}
       </div>
     )
   }
