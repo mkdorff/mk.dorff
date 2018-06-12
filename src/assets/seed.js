0db95312c7e85bf5b16fcc8837752068095ad8b0
mkdorff <mkdorff@outlook.com>
2018-06-12 10:58:56 -0600

    rsa & sudoku

--- src/assets/seed.js
+++ src/assets/seed.js
@@ -10,6 +10,8 @@ import ML from './ML'
 import Windo from './Windo'
 import Asteroids from './Asteroids'
 import Physics from './Physics'
+import RSA from './RSA'
+import Sudoku from './Sudoku'
 
 export default [
   {
@@ -31,50 +33,16 @@ export default [
   Windo,
   Asteroids,
   Physics,
-  {
-    shortName: 'RSA',
-    longName: 'RSA in JS & E-LISP',
-    sourceCode: 'https://github.com/mkdorff/RSA.js',
-    description: 'ALSO: Look here: https://github.com/mkdorff/CS237/blob/master/wellness.org'
-  },
-  {
-    shortName: 'Sudoku',
-    longName: 'Sudoku',
-    sourceCode: 'https://github.com/mkdorff/Sudoku-Solver',
-    description: 'And here is where we go ham with the text. Ham ham ham'
-  },
+  RSA,
+  Sudoku,
   {
     shortName: 'Manga!',
     longName: 'Manga Web Scraper',
     sourceCode: 'https://github.com/mkdorff/kissmanga-scraper',
     description: 'And here is where we go ham with the text. Ham ham ham'
-  },
-  {
-    shortName: 'Stats',
-    longName: 'Student Stats in Google Sheets',
-    description: 'This one isn\'t exciting at all.'
   }
   // TO ADD WHEN FINISHED:
   // OPOF
   // FACE MORPH
   // PARALLAX
 ]
-
-// Possible Fields
-// {
-//   shortName,
-//   longName,
-//   homepage,
-//   sourceCode,
-//   description: [],
-//   invertBackground
-//   images: [
-//     {
-//       image,
-//       caption,
-//       width,
-//       height
-//     }
-//   ],
-//   videos: []
-// }
