fa92313fadb35fa21174b4477229c1e88072226a
mkdorff <mkdorff@outlook.com>
2018-06-13 18:05:23 -0600

    some prelim arrow stuff

--- src/components/Arrows/index.js
+++ src/components/Arrows/index.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import LeftArrow from './LeftArrow.png'
+import RightArrow from './RightArrow.png'
 import './arrows.css'
 
-export default function Arrows({seed, active}) {
+export default function Arrows({seed, showArrows, onActiveChange}) {
   return (
     <div className='arrows'>
-      arrows
+      <div className="left-arrow">
+        <img src={LeftArrow} alt="" srcset=""/>
+      </div>
+      <div className="right-arrow">
+        <img src={RightArrow} alt="" srcset=""/>
+      </div>
     </div>
   )
 }
